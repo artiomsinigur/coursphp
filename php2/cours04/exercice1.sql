09b02147f2b3d44bf0fb08286d8ce446e92a70f9
Sinigur <e1895518@b1128-05.local>
2019-03-12 12:04:54 -0400

    AJout de cours03 et cours04

---
+++ php2/cours04/exercice1.sql
@@ -0,0 +1,21 @@
+CREATE TABLE proprietaire (
+    id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50),
+    nbPermis CHAR(15) UNIQUE NOT NULL
+);
+
+CREATE TABLE voiture (
+    id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    marque VARCHAR(20) NOT NULL,
+    modele VARCHAR(20) NOT NULL,
+    annee SMALLINT NOT NULL
+);
+
+CREATE TABLE achat (
+    prixAchat DECIMAL(6,2) UNSIGNED NOT NULL,
+    dateAchat DATE NOT NULL PRIMARY KEY,
+    idVoiture SMALLINT UNSIGNED PRIMARY KEY,
+    idProprietaire SMALLINT UNSIGNED PRIMARY KEY,
+    FOREIGN KEY(idProprietaire, idVoiture) REFERENCES proprietaire(id), voiture(id) ON DELETE CASCADE
+);
