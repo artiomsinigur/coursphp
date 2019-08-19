37bf418e7a50379e545246d06a30829287bc2859
Artiom S <sinigur2006@gmail.com>
2019-08-19 10:55:32 -0400

    Ajout de la method autoload avec namespace dans autoloader.php

--- autres/autoloader.php
+++ autres/autoloader.php
@@ -24,3 +24,24 @@
     // Appeler la function autolod de la class Autoloader
     require_once('class/Autoloader.php');
     Autoloader::register(); 
+
+
+    // ========================//
+    // Aoutoload avec namespace
+    // ========================//
+    namespace App;
+
+    class Autoloader {
+        static function register() {
+            // Ecrire dans le tableau le nom de la class et celui de la founction
+            // Récuperer dinamiquement le nom de la class avec "__CLASS__"
+            spl_autoload_register(array(__CLASS__, 'autoload'));
+        }
+
+        static function autoload($class) {
+            // Le __NAMESPACE__ récuperer le nom de la namespace
+            $class = str_replace(__NAMESPACE__ . '\\', '', $class);
+            var_dump($class);
+            require __DIR__ . "/" . $class . ".php";
+        }
+    }
